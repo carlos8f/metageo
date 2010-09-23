51a57be39cb0dcf7e0cd815c52183d2423d745df
Carlos Rodriguez <carlos@s8f.org>
2010-09-23 15:10:06 -0700

    Added geonames geocode support. remove command only works through cli. Abstracted metageo_save_feature() and allow input features to have _id attribute, which results in feature being updated.

--- metageo.php
+++ metageo.php
@@ -1,83 +1,85 @@
-#!/usr/bin/php
 <?php
 
-define('GEOCODE_URL', 'http://maps.google.com/maps/api/geocode/json?sensor=false&address=');
 ini_set('memory_limit', -1);
 
-if (!extension_loaded('mongo')) exit("requires mongo extension.\n");
+if (!extension_loaded('mongo')) metageo_exit("requires mongo extension.");
 
 $mongo = new Mongo();
-if (!$mongo) exit("couldn't connect to mongo!\n");
+if (!$mongo) metageo_exit("couldn't connect to mongo!");
 
-$prog = basename(array_shift($argv), '.php');
+if (isset($argv[0])) {
+  $prog = basename(array_shift($argv), '.php');
+}
+else {
+  $prog = basename(__FILE__, '.php');
+}
 $db = $mongo->$prog;
 
-if ($argc < 2) {
-  exit("usage: $prog command [options]\n");
-}
+metageo_parse_args();
 
-$command = array_shift($argv);
-$args = metageo_parse_args($argv);
+if (empty($command)) {
+  metageo_exit("usage: $prog command [options]");
+}
 
 switch ($command) {
   case 'insert': metageo_insert($args); break;
   case 'find': metageo_find($args); break;
   case 'remove': metageo_remove($args); break;
+  default: metageo_exit("invalid command.");
 }
 
 function metageo_insert($args) {
   global $prog, $db;
-  if (empty($args['file'])) exit("must specify --file=inputfile\n");
-  if (empty($args['name'])) exit("must specify --name=metadataname\n");
-  if (!file_exists($args['file'])) exit("--file does not exist.\n");
-  if (!empty($args['convert'])) {
-    exec('whereis ogr2ogr', $output, $ret);
-    if ($ret || empty($output) || !preg_match('~ogr2ogr: [^\w]~', $output[0])) exit("ogr2ogr command not found.\n");
-    $tmp = sys_get_temp_dir() .'/'. $prog .'_'. md5(time() . mt_rand());
-    exec(sprintf('ogr2ogr %s -f "GeoJSON" %s', escapeshellarg($tmp), escapeshellarg($args['file'])), $output, $ret);
-    if ($ret) exit("unable to convert input file.\n");
-    $args['file'] = $tmp;
-  }
-  $raw = file_get_contents($args['file']);
-  $input = json_decode($raw, TRUE);
-  if (empty($input)) {
-    $raw = utf8_encode($raw);
+  if (empty($args['file']) && empty($args['input'])) metageo_exit("must specify --file=inputfile or --input={geojson}");
+  if (empty($args['name'])) metageo_exit("must specify --name=metadataname");
+  if (!empty($args['file'])) {
+    if (!file_exists($args['file'])) metageo_exit("--file does not exist.");
+    if (!empty($args['convert'])) {
+      exec('whereis ogr2ogr', $output, $ret);
+      if ($ret || empty($output) || !preg_match('~ogr2ogr: [^\w]~', $output[0])) metageo_exit("ogr2ogr command not found.");
+      $tmp = sys_get_temp_dir() .'/'. $prog .'_'. md5(time() . mt_rand());
+      exec(sprintf('ogr2ogr %s -f "GeoJSON" %s', escapeshellarg($tmp), escapeshellarg($args['file'])), $output, $ret);
+      if ($ret) metageo_exit("unable to convert input file.");
+      $args['file'] = $tmp;
+    }
+    $raw = file_get_contents($args['file']);
     $input = json_decode($raw, TRUE);
+    if (empty($input)) {
+      $raw = utf8_encode($raw);
+      $input = json_decode($raw, TRUE);
+    }
   }
-  if (empty($input) || empty($input['features'])) exit("unable to parse input.\n");
+  elseif (!empty($args['input'])) {
+    $input = json_decode($args['input'], TRUE);
+  }
+  if (empty($input) || empty($input['features'])) metageo_exit("unable to parse input.");
  
-  $features_stats = metageo_features_stats($input['features']);
-
   foreach ($input['features'] as $feature) {
-    $db->properties->insert($feature['properties']);
-    $db->geometries->insert($feature['geometry']);
-    $record = array(
-      'name' => $args['name'],
-      'type' => $feature['geometry']['type'],
-      'center' => $feature['center'],
-      'radius' => $feature['radius'],
-      'bounds' => $feature['bounds'],
-      'properties' => $feature['properties']['_id'],
-      'geometry' => $feature['geometry']['_id'],
-    );
-    $db->features->insert($record);
+    if ($feature['type'] == 'FeatureCollection') {
+      foreach ($feature['features'] as $sub_feature) {
+        metageo_save_feature($sub_feature, $args['name']);
+      }
+    }
+    else {
+      metageo_save_feature($feature, $args['name']);
+    }
   }
-  
-  $meta = $db->meta->findOne(array('name' => $args['name']));
-  if (!$meta) {
-    $meta = array('name' => $args['name'], 'type' => $features_stats['type'], 'feature_count' => $meta['feature_count'] + $features_stats['feature_count']);
-  }
-  else {
-    $meta['feature_count'] = $features_stats['feature_count'];
-  }
-  $db->meta->save($meta);
 
   $db->features->ensureIndex(array('center' => '2d', 'name' => 1));
   $db->features->ensureIndex(array('name' => 1));
 
-  $db->meta->ensureIndex(array('name' => 1));
-
   if (!empty($args['convert'])) @unlink($tmp);
+  metageo_exit('insert ok.', 0);
+}
+
+function metageo_save_feature($feature, $name) {
+  global $db;
+  $feature += metageo_geometry_stats($feature['geometry']);
+  $feature['name'] = $name;
+  if (isset($feature['_id'])) {
+    $feature['_id'] = new MongoId($feature['_id']);
+  }
+  $db->features->save($feature);
 }
 
 function metageo_geometry_stats($geometry) {
@@ -90,8 +92,8 @@ function metageo_geometry_stats($geometry) {
     case 'MultiPolygon': foreach ($geometry['coordinates'] as $metagroup) foreach ($metagroup as $group) foreach ($group as $point) metageo_expand_bounds($point, $bounds); break;
   }
   $center = array(($bounds['min_x'] + $bounds['max_x']) / 2, ($bounds['min_y'] + $bounds['max_y']) / 2);
-  $radius = max($bounds['max_x'] - $bounds['min_x'], $bounds['max_y'] - $bounds['min_y']) / 2;
-  return compact('bounds', 'center', 'radius');
+  $bbox = array($bounds['min_x'], $bounds['min_y'], $bounds['max_x'], $bounds['max_y']);
+  return compact('bbox', 'center');
 }
 
 function metageo_expand_bounds($point, &$bounds) {
@@ -100,29 +102,6 @@ function metageo_expand_bounds($point, &$bounds) {
   $bounds['min_y'] = min($point[1], $bounds['min_y']);
   $bounds['max_x'] = max($point[0], $bounds['max_x']);
   $bounds['max_y'] = max($point[1], $bounds['max_y']);
-}
-
-function metageo_features_stats(&$features) {
-  $stats = array(
-    'type' => NULL,
-    'feature_count' => 0,
-  );
-  foreach ($features as $i => $feature) {
-    if ($feature['type'] != 'Feature') {
-      continue;
-    }
-    $stats['feature_count']++;
-    $features[$i] += metageo_geometry_stats($feature['geometry']);
-    if (!$stats['type']) {
-      switch ($feature['geometry']['type']) {
-        case 'Polygon': case 'MultiPolygon': $stats['type'] = 'Polygon'; break;
-        case 'Point': case 'MultiPoint': $stats['type'] = 'Point'; break;
-        case 'LineString': case 'MultiLineString': $stats['type'] = 'Line'; break;
-        default: $stats['type'] = 'Mixed'; break;
-      }
-    }
-  }
-  return $stats;
 }
 
 function metageo_find($args) {
@@ -141,7 +120,7 @@ function metageo_find($args) {
         $args['location'] = $geocode;
       }
       else {
-        exit("unable to geocode location.\n");
+        metageo_exit("unable to geocode location.");
       }
     }
     $conditions += array('center' => array('$near' => $args['location']));
@@ -149,40 +128,49 @@ function metageo_find($args) {
   if (!empty($args['limit'])) {
     $args['limit'] = (int) $args['limit'];
   }
-  else {
-    $args['limit'] = 100;
-  }
   if (!empty($args['name'])) {
-    $meta = $db->meta->findOne(array('name' => $args['name']));
-    if (!$meta) {
-      exit("--name not in database.\n");
+    if (strpos($args['name'], ',') !== FALSE) {
+      $conditions += array('name' => array('$in' => explode(',', $args['name'])));
     }
-    $conditions += array('name' => $args['name']);
+    else {
+      $conditions += array('name' => $args['name']);
+    }
+  }
+  if (!empty($args['conditions'])) {
+    if (!$extra_cond = json_decode($args['conditions'], TRUE)) {
+      metageo_exit("unable to parse --conditions.");
+    }
+    $conditions += $extra_cond;
   }
 
   $ret = array();
 
   if (!empty($args['within'])) {
-    $ret['within'] = metageo_find_within($conditions, $args['limit']);
+    $ret = metageo_find_within($conditions);
   }
-
-  if ((empty($args['within']) && empty($args['nearby'])) || !empty($args['nearby'])) {
-    $result = $db->features->find($conditions)->limit($args['limit']);
+  else {
+    $result = $db->features->find($conditions);
+    if (!empty($args['limit'])) {
+      $result->limit($args['limit']);
+    }
     foreach ($result as $feature) {
-      $feature['properties'] = $db->properties->findOne(array('_id' => $feature['properties']));
-      if (!empty($args['geometry'])) {
-        $feature['geometry'] = $db->geometries->findOne(array('_id' => $feature['geometry']));
+      if (!empty($args['location'])) {
+        $feature['distance'] = distance($args['location'], $feature['center']);
+        if (!empty($args['distance']) && $feature['distance'] > $args['distance']) {
+          break;
+        }
       }
-      else {
+      if (empty($args['geometry'])) {
         unset($feature['geometry']);
       }
-      $ret['nearby'][] = $feature;
+      $feature['_id'] .= '';
+      $ret[] = $feature;
     }
   }
-  var_dump($ret);
+  metageo_response($ret);
 }
 
-function metageo_find_within($conditions, $master_limit = 100) {
+function metageo_find_within($conditions) {
   global $db, $args;
 
   $reader = metageo_wkt_reader();
@@ -192,10 +180,13 @@ function metageo_find_within($conditions, $master_limit = 100) {
   $out_of_bounds = 0;
   $point = $conditions['center']['$near'];
   $g1 = $reader->read(sprintf('POINT(%f %f)', $point[0], $point[1]));
+
+  $names = (isset($args['name'])) ? array_flip(explode(',', $args['name'])) : FALSE;
+
   do {
-    $result = $db->features->find($conditions + array('type' => array('$in' => array('Polygon', 'MultiPolygon'))))->limit($limit)->skip($skip);
+    $result = $db->features->find($conditions + array('geometry.type' => array('$in' => array('Polygon', 'MultiPolygon'))))->limit($limit)->skip($skip);
     foreach ($result as $feature) {
-      if ($point[0] < $feature['bounds']['min_x'] || $point[0] > $feature['bounds']['max_x'] || $point[1] < $feature['bounds']['min_y'] || $point[1] > $feature['bounds']['max_y']) {
+      if ($point[0] < $feature['bbox'][0] || $point[0] > $feature['bbox'][2] || $point[1] < $feature['bbox'][1] || $point[1] > $feature['bbox'][3]) {
         // Point is out of bounds of this feature.
         $out_of_bounds++;
         if ($out_of_bounds - count($ret) == $limit) {
@@ -206,16 +197,22 @@ function metageo_find_within($conditions, $master_limit = 100) {
       }
       else {
         // Test that the point is within the polygon.
-        $feature['geometry'] = $db->geometries->findOne(array('_id' => $feature['geometry']));
         $wkt = metageo_geometry_to_wkt($feature['geometry']);
         $g2 = $reader->read($wkt);
         if ($g1->within($g2)) {
-          $feature['properties'] = $db->properties->findOne(array('_id' => $feature['properties']));
           if (empty($args['geometry'])) {
             unset($feature['geometry']);
           }
+          $feature['_id'] .= '';
           $ret[] = $feature;
-          if (count($ret) == $master_limit) {
+          if ($names) {
+            unset($names[$feature['name']]);
+          }
+          // If all names have been found, return result.
+          if ($names === array()) {
+            return $ret;
+          }
+          elseif (!empty($args['limit']) && count($ret) == $args['limit']) {
             return $ret;
           }
         }
@@ -229,9 +226,9 @@ function metageo_find_within($conditions, $master_limit = 100) {
 function metageo_wkt_reader() {
   static $reader;
   if (isset($reader)) return $reader;
-  if (!extension_loaded('geos')) exit("requires geos extension.\n");
+  if (!extension_loaded('geos')) metageo_exit("requires geos extension.");
   if (!$reader = new GEOSWKTReader()) {
-    exit("unable to initialize WKT reader.\n");
+    metageo_exit("unable to initialize WKT reader.");
   }
   return $reader;
 }
@@ -274,39 +271,119 @@ function metageo_geometry_to_wkt($geometry) {
 
 function metageo_remove($args) {
   global $db;
-  if (empty($args['name'])) exit("--name required.\n");
-  $result = $db->features->find(array('name' => $args['name']));
-  foreach ($result as $feature) {
-    $db->properties->remove(array('_id' => $feature['properties']));
-    $db->geometries->remove(array('_id' => $feature['geometry']));
-    $db->features->remove(array('_id' => $feature['_id']));
+  if (!metageo_is_cli()) {
+    metageo_exit("remove command only available from cli.\n");
   }
+  if (empty($args['name'])) metageo_exit("--name required.");
+  $db->features->remove(array('name' => $args['name']));
 }
 
 function metageo_geocode($location) {
-  if ($geocode = json_decode(file_get_contents(GEOCODE_URL . urlencode($location)))) {
-    if ($geocode->status == 'OK') {
-      foreach ($geocode->results as $result) {
-        return array($result->geometry->location->lng, $result->geometry->location->lat);
+  global $prog;
+  // Try Google first...
+  if ($geocode = json_decode(file_get_contents('http://maps.google.com/maps/api/geocode/json?sensor=false&address=' . urlencode($location)), TRUE)) {
+    if ($geocode['status'] == 'OK') {
+      foreach ($geocode['results'] as $result) {
+        return array($result['geometry']['location']['lng'], $result['geometry']['location']['lat']);
+      }
+    }
+  }
+  // Try geonames...
+  if ($geocode = json_decode(file_get_contents('http://ws.geonames.org/searchJSON?username='. $prog .'&q='. urlencode($location)), TRUE)) {
+    if (!empty($geocode['geonames'])) {
+      foreach ($geocode['geonames'] as $result) {
+        return array($result['lng'], $result['lat']);
+      }
+    }
+  }
+  return FALSE;
+}
+
+function metageo_parse_args() {
+  global $command, $args;
+  $args = array();
+  if (metageo_is_cli()) {
+    global $argv;
+    $command = array_shift($argv);
+    foreach ($argv as $arg) {
+      $arg = trim($arg, '-');
+      if (strpos($arg, '=') !== FALSE) {
+        list($key, $value) = explode('=', $arg);
+        $args[$key] = $value;
+      }
+      else {
+        $args[$arg] = TRUE;
       }
     }
   }
   else {
-    return FALSE;
+    if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
+      $quotes_sybase = strtolower(ini_get('magic_quotes_sybase'));
+      $unescape_function = (empty($quotes_sybase) || $quotes_sybase === 'off') ? 'stripslashes($value)' : 'str_replace("\'\'","\'",$value)';
+      $stripslashes_deep = create_function('&$value, $fn', '
+          if (is_string($value)) {
+              $value = ' . $unescape_function . ';
+          } else if (is_array($value)) {
+              foreach ($value as &$v) $fn($v, $fn);
+          }
+      ');
+
+      // Unescape data
+      $stripslashes_deep($_POST, $stripslashes_deep);
+      $stripslashes_deep($_GET, $stripslashes_deep);
+      $stripslashes_deep($_COOKIE, $stripslashes_deep);
+      $stripslashes_deep($_REQUEST, $stripslashes_deep);
+    }
+    $args = $_GET;
+    $command = isset($_GET['command']) ? $_GET['command'] : FALSE;
   }
 }
 
-function metageo_parse_args($args) {
-  $retval = array();
-  foreach ($args as $arg) {
-    $arg = trim($arg, '-');
-    if (strpos($arg, '=') !== FALSE) {
-      list($key, $value) = explode('=', $arg);
-      $retval[$key] = $value;
+/**
+ * hocus pocus!
+ */
+function distance($point1, $point2) {
+  $theta = $point1[0] - $point2[0];
+  $dist = sin(deg2rad($point1[1])) * sin(deg2rad($point2[1])) +  cos(deg2rad($point1[1])) * cos(deg2rad($point2[1])) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  $miles = $dist * 60 * 1.1515;
+  return $miles;
+}
+
+function metageo_response($resp) {
+  global $args;
+  if (metageo_is_cli()) {
+    var_dump($resp);
+  }
+  else {
+    header('Content-Type: text/plain; charset=utf-8');
+    if (isset($args['callback'])) {
+      print $args['callback'] ."(\n\t";
     }
-    else {
-      $retval[$arg] = TRUE;
+    print json_encode($resp);
+    if (isset($args['callback'])) {
+      print "\n);";
     }
   }
-  return $retval;
+}
+
+function metageo_exit($message, $status = 1) {
+  if (metageo_is_cli()) {
+    print $message ."\n";
+    exit($status);
+  }
+  else {
+    if ($status) {
+      metageo_response(array('ok' => FALSE, 'message' => $message));
+    }
+    else {
+      metageo_response(array('ok' => TRUE, 'message' => $message));
+    }
+    exit();
+  }
+}
+
+function metageo_is_cli() {
+  return php_sapi_name() === 'cli';
 }
