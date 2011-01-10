37f1fd39b3163ba30e99d6118d4a2450b74bd1cf
Carlos Rodriguez <carlos@s8f.org>
2011-01-10 19:32:43 +0000

    Added support for defining mongo connection URL in config.php (METAGEO_CONNECTION_STRING).

--- metageo.php
+++ metageo.php
@@ -7,7 +7,8 @@ ini_set('memory_limit', -1);
 
 if (!extension_loaded('mongo')) metageo_exit("requires mongo extension.");
 
-$mongo = new Mongo();
+$connection_string = defined('METAGEO_CONNECTION_STRING') ? METAGEO_CONNECTION_STRING : 'mongodb://localhost';
+$mongo = new Mongo($connection_string);
 if (!$mongo) metageo_exit("couldn't connect to mongo!");
 
 if (isset($argv[0])) {
